4ea392e325e16620df4f38a17a9fe84e3461d7e6
twoelves <65972796+twoelves@users.noreply.github.com>
2021-01-21 17:51:50 +0000

    Update prufa.js

--- pages/prufa.js
+++ pages/prufa.js
@@ -1,3 +1,27 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Velkomin á þessa síðu!" />
+        <p className="description">
+          Byrjum á að breyta <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
+
 const Prismic = require('@prismicio/client')
 
 Prismic.client("https://prufutourdesk.cdn.prismic.io/api/v2",  { req: req }).then(function(api) {  return api.query(
