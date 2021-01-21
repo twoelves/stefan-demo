7c297cd9b8eda64ec6dfdc8219fd2e9cff4e4c9c
twoelves <65972796+twoelves@users.noreply.github.com>
2021-01-21 17:27:12 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Velkomin á þessa síðu!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Byrjum á að breyta <code>pages/index.js</code>
         </p>
       </main>
 
