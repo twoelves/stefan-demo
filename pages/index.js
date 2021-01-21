ab43052b7038b7b5b08484911271a7dd1e3f5209
twoelves <65972796+twoelves@users.noreply.github.com>
2021-01-21 17:54:16 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,17 @@ export default function Home() {
         <p className="description">
           Byrjum á að breyta <code>pages/index.js</code>
         </p>
+    
+const Prismic = require('@prismicio/client')
+
+Prismic.client("https://prufutourdesk.cdn.prismic.io/api/v2",  { req: req }).then(function(api) {  return api.query(
+    Prismic.Predicates.at('document.type', 'blog-post'),
+    { orderings : '[my.blog-post.date desc]' }
+  );
+}).then(function(response) {
+  // response is the response object, response.results holds the documents
+});
+
       </main>
 
       <Footer />
